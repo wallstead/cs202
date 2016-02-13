e8a93defae7bf4cb86f4c50ae5069ac22230e337
Willis Allstead <willallstead@icloud.com>
2016-02-13 12:41:01 -0800

    Now fin retrieves the pieceCount and keyCounts from the file.

--- project4/project4.cpp
+++ project4/project4.cpp
@@ -6,9 +6,22 @@ using namespace std;
 struct Pieces {
     char *word;
     int jump;
-}
+};
 
 int main() {
-    cout << "hello" << endl;
+    ifstream fin;
+    fin.open("data");
+    int keyCount, pieceCount;
+    if(fin.is_open()) {
+        cout << "opened" << endl;
+        fin >> pieceCount >> keyCount;
+    }
+
+    int *keyptr = new int[ikeyCount]; // Keys
+    *keyptr = 10;
+    cout << *keyptr << endl;
+
+
+
     return 0;
 }
