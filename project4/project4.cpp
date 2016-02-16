8128ef0d8bfadd725d5152b440114daded57cc1d
Willis Allstead <willallstead@icloud.com>
2016-02-16 08:31:29 -0800

    memory is allocated for keycount and piececount now

--- project4/project4.cpp
+++ project4/project4.cpp
@@ -8,8 +8,8 @@ struct Pieces {
     int jump;
 };
 
-void readFile(Pieces *&pieceptr, int *&keyptr);
-void decodeMessage(Pieces *pieceptr, int *keyptr);
+void readFile(Pieces *pieceptr, int *keyptr, int *pieceCount, int *keyCount);
+void decodeMessage(Pieces *pieceptr, int *keyptr, int *pieceCount, int *keyCount);
 int stringLength(char *str);
 void stringCopy(char *str1, char *str2);
 
@@ -17,55 +17,67 @@ int main() {
     Pieces *pieceptr; // Points to first piece in piece array.
     int *keyptr; // Points to first key in key array.
     char *decoded; // Decoded message
+    int *pieceCount;
+    int *keyCount;
+    readFile(pieceptr, keyptr, pieceCount, keyCount); // pass these Pointers to readFile function
 
+    cout << (*pieceptr).word << endl;
 
-
-    readFile(pieceptr, keyptr); // pass these Pointers to readFile function
-
-    decodeMessage(pieceptr, keyptr);
-    // cout << (*keyptr) << endl;
+    decodeMessage(pieceptr, keyptr, pieceCount, keyCount);
 
 
     return 0;
 }
 
-void readFile(Pieces *&pieceptr, int *&keyptr) {
+void readFile(Pieces *pieceptr, int *keyptr, int *pieceCount, int *keyCount) {
     char ifileName[20];
     cout << "Please enter file name: ";
     cin >> ifileName;
 
     ifstream fin;
     fin.open(ifileName);
-    int keyCount, pieceCount;
+
     if(fin.good()) {
         cout << "Opened File Successfully" << endl;
-        fin >> pieceCount >> keyCount;
+        /* make enough room for both of these variables. */
+        pieceCount = new int[5];
+        keyCount = new int[5];
 
-        pieceptr = new Pieces[pieceCount]; // Pieces
+        fin >> *pieceCount >> *keyCount;
+
+        cout << *pieceCount << " " << *keyCount;
+
+        pieceptr = new Pieces[*pieceCount]; // Pieces
         Pieces *pieceCopy = pieceptr;
-        keyptr = new int[keyCount]; // Keys
+        keyptr = new int[*keyCount]; // Keys
+        int *keyCopy = keyptr;
 
-        for (int i = 0; i < pieceCount; i++) {
+        for (int i = 0; i < *pieceCount; i++) {
             char *tempWord = new char[20]; // create pointer to first char in char array.
             fin >> tempWord >> (*pieceCopy).jump; // Because tempWord actually returns the string it points to, not the address of the pointer
             (*pieceCopy).word = new char[stringLength(tempWord)]; // Create exact amount of memory needed for word.
             stringCopy((*pieceCopy).word, tempWord);
             delete []tempWord; // Done with tempWord
             tempWord = NULL;
+            cout << (*pieceptr).word << endl;
             pieceCopy++;
         }
 
-        for (int j = 0; j < keyCount; j++) {
-            fin >> *keyptr;
-            keyptr++;
+        for (int j = 0; j < *keyCount; j++) {
+            fin >> *keyCopy;
+            keyCopy++;
         }
     } else { cout << "Cannot opening file." << endl; }
     fin.close();
 }
 
-void decodeMessage(Pieces *pieceptr, int *keyptr) {
-    pieceptr++;
-    cout << (*pieceptr).word << endl;
+void decodeMessage(Pieces *pieceptr, int *keyptr, int *pieceCount, int *keyCount) {
+    /* Make copies of both Pointers */
+    Pieces *pieceCopy = pieceptr;
+    int *keyCopy = keyCopy;
+    // cout << (*pieceptr).word << endl;
+
+
 }
 
 int stringLength(char *str) {
@@ -79,6 +91,9 @@ int stringLength(char *str) {
 
 void stringCopy(char *str1, char *str2) {
     while (*str2 != '\0') {
-         *str1++ = *str2++;
+         *str1 = *str2;
+         str1++;
+         str2++;
     }
+    *str1 = '\0';
 }
