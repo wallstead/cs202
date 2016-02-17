251aa01b83860ba3eaff794fc8b0ad0c4c689f58
Willis Allstead <willallstead@icloud.com>
2016-02-17 15:30:45 -0800

    Memory fixes done.

--- project4/project4.cpp
+++ project4/project4.cpp
@@ -15,13 +15,14 @@ void strConcat(char *str1, char *str2);
 void stringCopy(char *str1, char *str2);
 
 int main() {
-    /* Prompt user for file name */
     char *ifileName = new char[20];
     cout << "Please enter file name: ";
     cin >> ifileName;
 
     ifstream fin;
-    fin.open(ifileName);
+    fin.open(ifileName); // open the file
+    delete []ifileName; // Done with file
+    ifileName = NULL;
 
     int pieceCount, keyCount;
 
@@ -33,10 +34,9 @@ int main() {
         cout << "Error opening file." << endl;
     }
 
-    // cout << pieceCount << " " << keyCount << endl;
-
     /* Allocate arrays */
     Pieces *pptr = new Pieces[pieceCount];
+    Pieces *pptrHome = pptr; // Don't move this!
     int *kptr = new int[keyCount];
     char *decrypted = new char[100]; // temporarily allocate room for 100 chars
 
@@ -46,35 +46,59 @@ int main() {
     /* Decrypt data */
     decrypt(decrypted, pptr, kptr, pieceCount, keyCount);
 
+    /* delete allocated memory for each word in pieces */
+    for (int i = 0; i < pieceCount; i++) {
+        delete [](*pptr).word;
+        (*pptr).word = NULL;
+        pptr++;
+    }
 
+    /* delete allocated memory for pieces */
+    pptr = pptrHome;
+    delete []pptr;
+    pptr = NULL;
 
-    cout << "FINISHED: " << decrypted << endl;
+    /* delete allocated memory for keys  */
+    delete []kptr;
+    kptr = NULL;
+
+    /* resize decrypted string */
+
+    char *decrypted_final = new char[stringLength(decrypted)+1]; // Create exact amount of memory needed for word.
+    stringCopy(decrypted_final, decrypted);
+    delete []decrypted;
+    decrypted = NULL;
+
+    cout << "FINISHED: " << decrypted_final << endl;
+
+    /* delete allocated memory for decrypted string.  */
+    delete []decrypted_final;
+    decrypted_final = NULL;
 
     return 0;
 }
 
 void readFile(ifstream *fin, Pieces *pptr, int *kptr, int pieceCount, int keyCount) {
-    Pieces *pptrHome = pptr;
-    int *kptrHome = kptr;
+    Pieces *pptrHome = pptr; // Don't move this home pointer
+    int *kptrHome = kptr; // Don't move this home pointer
 
     for (int i = 0; i < pieceCount; i++) {
         char *tempWord = new char[20]; // create pointer to first char in char array.
-        *fin >> tempWord >> (*pptr).jump; // Because tempWord actually returns the string it points to, not the address of the pointer
+        *fin >> tempWord >> (*pptr).jump; // Get the word and the jump
         (*pptr).word = new char[stringLength(tempWord)+1]; // Create exact amount of memory needed for word.
         stringCopy((*pptr).word, tempWord);
         delete []tempWord; // Done with tempWord
         tempWord = NULL;
-        // cout << (*pptr).word << endl;
         pptr++;
     }
 
     for (int j = 0; j < keyCount; j++) {
-        *fin >> *kptr;
+        *fin >> *kptr; // get the key
         kptr++;
     }
 
-    pptr = pptrHome;
-    kptr = kptrHome;
+    pptr = pptrHome; // reset to home
+    kptr = kptrHome; // reset to home
     (*fin).close();
 }
 
@@ -85,13 +109,14 @@ void decrypt(char *decrypted, Pieces *pptr, int *kptr, int pieceCount, int keyCo
     int pieceIndex; // to keep track of current position
 
     for (int i = 0; i < keyCount; i++) { //For each key
-        pptr += *kptr;
-        pieceIndex = *kptr;
-        while((*pptr).jump != 0) {
-            if ((pieceIndex + (*pptr).jump) < pieceCount) {
-                pptr += (*pptr).jump;
-                pieceIndex += (*pptr).jump;
+        pptr += *kptr; // set the pointer to the key count
+        pieceIndex = *kptr; // keep track with index
+        while((*pptr).jump != 0) { // stop only if jump is 0
+            if ((pieceIndex + (*pptr).jump) < pieceCount) { // if it doesnt wrap
+                pptr += (*pptr).jump; // jump the needed length
+                pieceIndex += (*pptr).jump; // keep track
             } else { //wrap
+                /* Calulate the new Index for the pptr */
                 int difference = pieceCount - pieceIndex;
                 int newIndex = difference - (*pptr).jump;
                 pptr = pptrHome + newIndex;
@@ -100,9 +125,8 @@ void decrypt(char *decrypted, Pieces *pptr, int *kptr, int pieceCount, int keyCo
         }
         strConcat(decrypted, (*pptr).word);
         kptr++;
-        pptr = pptrHome;
+        pptr = pptrHome; //reset to home
     }
-    cout << endl;
 }
 void strConcat(char *str1, char *str2) {
     /* Add str2 to the end of str1 */
