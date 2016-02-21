0e345d2faaf5113cf96064b0e657dee0499f019d
Willis Allstead <willallstead@icloud.com>
2016-02-21 13:21:02 -0800

    Test case for single strings finished.

--- project5/project5.cpp
+++ project5/project5.cpp
@@ -5,31 +5,68 @@
 using namespace std;
 
 int main() {
-    char *word = new char[20];
+    ifstream fin;
+    fin.open("strings");
 
-    cout << "Enter a string: ";
-    cin >> word;
+    if (fin.is_open()) {
+        // cout << "Opened file." << endl;
 
-    String test;
-    test.initialize(word);
-    test.print();
+        String single1, single2;
 
-    char *word2 = new char[20];
+        char *temp1 = new char[20];
+        char *temp2 = new char[20];
+        fin >> temp1 >> temp2;
 
-    cout << "Enter a string 2: ";
-    cin >> word2;
+        single1.initialize(temp1);
+        single2.initialize(temp2);
+        delete []temp1;
+        temp1 = NULL;
+        delete []temp2;
+        temp2 = NULL;
 
-    String test2;
-    test2.initialize(word2);
+        cout << "Buffer of both strings: " << endl;
+        cout << "\tstring 1: ";
+        single1.print();
+        cout << "\tstring 2: ";
+        single2.print();
+
+        cout << "Length of both strings: " << endl;
+        cout << "\tstring 1: " << single1.length() << endl;
+        cout << "\tstring 2: " << single2.length() << endl;
+
+        cout << "Comparing string 2 to string 1: " << endl;
+        cout << "\t" <<single1.compare(single2) << endl;
+
+        cout << "Comparing string 1 to string 2: " << endl;
+        cout << "\t" <<single2.compare(single1) << endl;
+
+        cout << "Concatenating string 1 to string 2: " << endl;
+        single2.concat(single1);
+        cout << "\tstring 1: ";
+        single1.print();
+        cout << "\tstring 2: ";
+        single2.print();
+
+        cout << "Concatenating string 2 to string 1: " << endl;
+        single1.concat(single2);
+        cout << "\tstring 1: ";
+        single1.print();
+        cout << "\tstring 2: ";
+        single2.print();
+
+        cout << "Copying string 2 to string 1: " << endl;
+        single1.copy(single2);
+        cout << "\tstring 1: ";
+        single1.print();
+        cout << "\tstring 2: ";
+        single2.print();
+
+        cout << "Deallocating memory for both strings." << endl;
+        single1.deallocate();
+        single2.deallocate();
 
 
-    cout << test.compare(test2) << endl;
-
-    // ifstream fin;
-    // fin.open("strings");
-    // if (fin.good()) {
-    //     cout << "Opened file." << endl;
-    // } else {
-    //     cout << "Could not open file." << endl;
-    // }
+    } else {
+        cout << "Could not open file." << endl;
+    }
 }
