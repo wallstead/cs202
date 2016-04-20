7d2ff84634ddebee800b990cdc08f5ddfc35898a
Willis Allstead <willallstead@icloud.com>
2016-04-20 15:10:16 -0700

    Finished array implementation of List data structure.

--- project10/listarray.cpp
+++ project10/listarray.cpp
@@ -45,8 +45,18 @@ ListArray<T>::ListArray(int size) {
 }
 
 template <typename T>
-ListArray<T>::ListArray(const ListArray<T>&) {
-
+ListArray<T>::ListArray(const ListArray<T> &toCopy) {
+    if (this != &toCopy) {
+        this->data = new T[toCopy.size];
+        this->size = toCopy.size;
+        this->cursor = toCopy.cursor;
+        this->actual = toCopy.actual;
+        for (int i = 0; i < toCopy.actual; i++) {
+            this->data[i] = toCopy.data[i];
+        }
+    } else {
+        cout << "cannot copy self" << endl;
+    }
 }
 
 template <typename T>
@@ -60,12 +70,18 @@ ListArray<T>::~ListArray() {
 template <typename T>
 bool ListArray<T>::insertBefore(const T &toInsert) {
     if (!isFull()) {
-        cursor--;
-
         if (isEmpty()) {
-
+            cursor++;
+            data[cursor] = toInsert;
+            actual++;
         } else {
-
+            //shift everything to the right of cursor right one
+            for (int i = actual; i > cursor; i--) {
+                data[i] = data[i-1];
+            }
+            //set value of this one
+            data[cursor] = toInsert;
+            actual++;
         }
         return true;
     } else {
@@ -76,9 +92,15 @@ bool ListArray<T>::insertBefore(const T &toInsert) {
 template <typename T>
 bool ListArray<T>::insertAfter(const T &toInsert) {
     if (!isFull()) {
+        //shift everything to the right of cursor right one
+        for (int i = actual; i > cursor; i--) {
+            data[i] = data[i-1];
+        }
+        //move cursor
         cursor++;
-        actual++;
+        //set value of this one
         data[cursor] = toInsert;
+        actual++;
         return true;
     } else {
         return false;
@@ -86,13 +108,104 @@ bool ListArray<T>::insertAfter(const T &toInsert) {
 }
 
 template <typename T>
+bool ListArray<T>::get(T &toReturn) const {
+    if (!isEmpty()) {
+        toReturn = data[cursor];
+        return true;
+    } else {
+        return false;
+    }
+}
+
+template <typename T>
+bool ListArray<T>::remove(T &removed) {
+    if (!isEmpty()) {
+        removed = data[cursor];
+        //move everything right of cursor to the left one
+        for (int i = cursor; i < actual; i++) {
+            data[i] = data[i+1];
+        }
+        actual--;
+        // if cursor is now out of bounds move it left one
+        if (cursor == actual) {
+            cursor--;
+        }
+        return true;
+    } else {
+        return false;
+    }
+}
+
+template <typename T>
+bool ListArray<T>::goToBeginning() {
+    if (!isEmpty()) {
+        cursor = 0;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+template <typename T>
+bool ListArray<T>::goToEnd() {
+    if (!isEmpty()) {
+        cursor = actual-1;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+template <typename T>
+bool ListArray<T>::goToNext() {
+    if (!isEmpty()) {
+        if (cursor < actual-1) {
+            cursor++;
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
+
+template <typename T>
+bool ListArray<T>::goToPrior() {
+    if (!isEmpty()) {
+        if (cursor > 0) {
+            cursor--;
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
+
+template <typename T>
 bool ListArray<T>::isEmpty() const {
-    return actual == 0;
+    return (actual == 0);
 }
 
 template <typename T>
 bool ListArray<T>::isFull() const {
-    return actual == size;
+    return (actual == size);
+}
+
+template <typename T>
+ListArray<T>& ListArray<T>::operator=(const ListArray<T> &toCopy) {
+        delete [] this->data;
+        this->data = new T[toCopy.size];
+        this->size = toCopy.size;
+        this->cursor = toCopy.cursor;
+        this->actual = toCopy.actual;
+        for (int i = 0; i < toCopy.actual; i++) {
+            this->data[i] = toCopy.data[i];
+        }
+
+    return *this;
 }
 
 template <typename S>
--- project10/main.cpp
+++ project10/main.cpp
@@ -4,15 +4,38 @@
 using namespace std;
 
 int main() {
-    ListArray<int> list;
+    ListArray<int> list = ListArray<int>(5);
+    int removed;
     cout << list << endl;
-    list.insertAfter(1);
+    list.insertBefore(1);
     cout << list << endl;
-    list.insertAfter(2);
+    list.insertBefore(2);
     cout << list << endl;
     list.insertAfter(3);
     cout << list << endl;
     list.insertAfter(4);
     cout << list << endl;
+    list.insertBefore(5);
+    cout << list << endl;
+    list.goToNext();
+    cout << list << endl;
+    list.goToNext();
+    cout << list << endl;
+    list.goToNext();
+
+    ListArray<int> list2 = ListArray<int>(5);
+    cout << list2 << endl;
+    list2 = list;
+    cout << list2 << endl;
+    list2.goToNext();
+    cout << list2 << endl;
+    list2.goToBeginning();
+    cout << list2 << endl;
+    list2.remove(removed);
+    cout << list2 << endl;
+
+    ListArray<int> list3 = list2;
+    cout << list3 << endl;
+
     return 0;
 }
